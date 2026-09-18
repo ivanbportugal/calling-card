9c03c9847df2e0bb5a784893a5dfd68661268bcc
Ivan Portugal <ivanbportugal@gmail.com>
2026-09-17 22:28:51 -0500

    on resume, refresh data

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:calling_card/auth/user_repository.dart';
 import 'package:calling_card/firebase_options.dart';
+import 'package:calling_card/friends/friends_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'auth/push_token_manager.dart';
@@ -26,13 +29,21 @@ void main() async {
   runApp(UncontrolledProviderScope(container: container, child: const CallingCardApp()));
 }
 
-class CallingCardApp extends ConsumerWidget {
+class CallingCardApp extends HookConsumerWidget {
   const CallingCardApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
     final themeMode = ref.watch(themeNotifier);
+
+    useOnAppLifecycleStateChange((previous, current) {
+      if (current == AppLifecycleState.resumed) {
+        ref.refresh(userProfileProvider.future);
+        ref.refresh(friendsProvider.future);
+        ref.refresh(friendRequestsProvider.future);
+      }
+    });
 
     return MaterialApp.router(
       title: 'Calling Card',
